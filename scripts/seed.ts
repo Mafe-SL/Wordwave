ca0e7c870c5b831f9673a324d0f34fc648e4b5c6
Adalidh1 <adalidh141@gmail.com>
2024-07-15 12:21:37 -0600

    schema

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,11 @@ const main = async () => {
 
         await db.delete(schema.courses)
         await db.delete(schema.userProgress)
+        await db.delete(schema.units)
+        await db.delete(schema.lessons)
+        await db.delete(schema.challenges)
+        await db.delete(schema.challengeOptions)
+        await db.delete(schema.challengeProgress)
 
         await db.insert(schema.courses).values([
             {
@@ -41,6 +46,41 @@ const main = async () => {
                 title: "Italian",
                 imageSrc: "/it.svg"
             },
+        ])
+
+        await db.insert(schema.units).values([
+            {
+                id: 1,
+                courseId: 1, //spanish
+                title: "Unit 1",
+                description: "Learn the basics of Spanish",
+                order: 1
+            }
+        ])
+
+        await db.insert(schema.lessons).values([
+            {
+                id: 1,
+                unitId: 1,
+                order: 1,
+                title: "Nouns"
+            },
+            {
+                id: 2,
+                unitId: 1,
+                order: 2,
+                title: "Verbs"
+            },
+        ])
+
+        await db.insert(schema.challenges).values([
+            {
+                id: 1,
+                lessonId: 1,
+                order: 1,
+                type: "SELECT",
+                question: 'Which one of these is the "the man"?'
+            }
         ])
 
         console.log("Seeding finished")
